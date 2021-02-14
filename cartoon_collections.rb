e4c753eefa420df43ebaf51d9d124dea86d21962
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-14 05:03:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,12 @@
 def greet_characters(array)
  # count = 0
   array.each do |dwarves|
+    def greet_characters(array)
+ 
+  array.each do |dwaf|
+    #puts out greeting to dwarf
+  end
+end
   end
 end
   
@@ -17,3 +23,9 @@ def list_dwarves(array)
   # Print a numbered list of each element
 end
 
+def greet_characters(array)
+ 
+  array.each do |dwaf|
+    #puts out greeting to dwarf
+  end
+end
